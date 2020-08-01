32b906b1cf3e464ae4b9ff053323f4d134978c5d
Naseem khan <Naseemkhan702167@gmail.com>
2020-08-01 16:21:44 +0530

    To customize the appendDictasrow function

--- Code-a-thon_Competition/controller.py
+++ Code-a-thon_Competition/controller.py
@@ -1,13 +1,22 @@
 from csv import DictWriter, DictReader
 import pandas as pd
+import os
 
+# adding the data in csv file
 def append_dict_as_row(file_name, dict_of_elem, field_names):       #to creat the .csv file
+    # chacke for the file pracent or not 
+    file_ext = os.path.isfile(file_name)
+    
     # Open file in append mode
     with open(file_name, 'a+', newline='') as write_obj:
         # Create a writer object from csv module
         dict_writer = DictWriter(write_obj, fieldnames=field_names)
+        if file_ext == False:
+            dict_writer.writeheader()
+        
         # Add dictionary as wor in the csv
         dict_writer.writerow(dict_of_elem)
+
 
 
 def dataSoter(name,number,street,city,state,code):
@@ -54,5 +63,5 @@ def searchrecord():
 def searchrecord():
     data=pd.read_csv('eployeeDetails.csv')
     data = data.set_index('EmployeeNo')
-    empNo = int(input('Enter employee no => '))
-    return data.loc[empNo]
+    empName = int(input('Enter employee no => '))
+    return data.loc[empName]
